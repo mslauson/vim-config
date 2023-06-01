db614075da343bd21da631d7c119ed30a8d605b3
Matthew Slauson <matthew@slauson.io>
2023-06-01 11:56:32 -0500

    update

--- init.sh
+++ init.sh
@@ -4,13 +4,17 @@ echo "Do you want to init for root?"
 select yn in "Yes" "No" "Cancel"; do
     case $yn in
         Yes ) 
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim /home/root/.config/nvim
+            echo "Initializing for root and $USER"
+            rm -rf /home/root/.config/nvim
+            git clone --depth 1 https://github.com/AstroNvim/AstroNvim /home/root/.config/nvim
             ln -s "$currentPath"/user ~/.config/nvim/lua/user
             rm -rf ~/.config/nvim/lua/user
             rm -rf /home/root/.vimrc
             ln -s "$CurrentPath"/vimrc /home/root/.vimrc
             break;;
-        No ) break;;
+        No )
+            echo "Initializing for $USER"
+            break;;
         Cancel ) 
             echo "No config was changed!"
             exit
@@ -18,6 +22,7 @@ git clone --depth 1 https://github.com/AstroNvim/AstroNvim /home/root/.config/nv
     esac
 done
    
+rm -rf ~/.config/nvim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 rm -rf ~/.config/nvim/lua/user
 ln -s "$currentPath"/user ~/.config/nvim/lua/user

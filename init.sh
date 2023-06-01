46f54da6a3ed5f18df0811c3a86ccca770bc07d6
Matthew Slauson <matthew@slauson.io>
2023-06-01 11:41:52 -0500

    update

--- init.sh
+++ init.sh
@@ -1,6 +1,24 @@
 currentPath="$(pwd)"
-    rm -rf ~/.config/nvim/lua/user
-    ln -s "$currentPath"/user ~/.config/nvim/lua/user
     
-    rm -rf ~/.vimrc
-    ln -s "$CurrentPath"/vimrc ~/.vimrc
+echo "Do you want to init for root?"
+select yn in "Yes" "No" "Cancel"; do
+    case $yn in
+        Yes ) 
+            ln -s "$currentPath"/user ~/.config/nvim/lua/user
+            rm -rf ~/.config/nvim/lua/user
+            rm -rf /home/root/.vimrc
+            ln -s "$CurrentPath"/vimrc /home/root/.vimrc
+            break;;
+        No ) break;;
+        Cancel ) 
+            echo "No config was changed!"
+            exit
+            ;;
+    esac
+done
+   
+rm -rf ~/.config/nvim/lua/user
+ln -s "$currentPath"/user ~/.config/nvim/lua/user
+
+rm -rf ~/.vimrc
+ln -s "$CurrentPath"/vimrc ~/.vimrc

c0296630680a904e68a73911f8381011a07f31ba
Matthew Slauson <matthew@slauson.io>
2023-06-01 12:00:41 -0500

    fix

--- init.sh
+++ init.sh
@@ -8,7 +8,9 @@ select yn in "Yes" "No" "Cancel"; do
             sudo rm -rf /home/root/.config/nvim
             sudo rm -rf /home/root/.vimrc
             
+            
             sudo git clone --depth 1 https://github.com/AstroNvim/AstroNvim /home/root/.config/nvim
+            ln -s "$currentPath"/user /home/root/.config/nvim/lua/user
             sudo ln -s "$CurrentPath"/vimrc /home/root/.vimrc
             break;;
         No )
